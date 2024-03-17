5314a295cb5a46a695d1be2d486893532ba1d802
Felix <felixgammal1@hotmail.com>
2024-03-17 11:58:39 +0100

    signup init

--- SiliconAssignment/Controllers/AuthController.cs
+++ SiliconAssignment/Controllers/AuthController.cs
@@ -4,5 +4,9 @@ namespace SiliconAssignment.Controllers;
 
 public class AuthController : Controller
 {
-
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
 }
